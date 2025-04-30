bcd92fb49b882f3ca9b6819a5f0eb8d5a2e4deeb
AlessioZanna <157384252+AlessioZanna@users.noreply.github.com>
2025-04-30 19:24:55 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,23 @@
-const CACHE_NAME = "music-notes-cache-v1";
+const CACHE_NAME = 'team5-cache-v1';
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/script.js",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
-// Installazione del Service Worker
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log("Caching delle risorse");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Attivazione del Service Worker
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log("Eliminazione cache vecchia:", cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Gestione delle richieste
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
